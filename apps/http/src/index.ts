f5e7756ea79f364b39df4a1d327acfb5ffcfc90f
arvindkhoisnam <khoisnamarvind@gmail.com>
2025-01-30 02:13:59 +0530

    cors_update

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -20,22 +20,10 @@ export const redisSubscriber = createClient({
   await redisSubscriber.connect();
   redisClient.on("error", (err) => console.log("Redis Client Error", err));
   console.log("Redis connected");
-  console.log("Redis pub connected");
-  console.log("Redis sub connected");
 })();
 
 const app = express();
 app.use(express.json());
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: [
-//       "http://localhost:5173",
-//       "http://192.168.29.143:5173",
-//       "https://arvind-live-classes.netlify.app",
-//     ],
-//   })
-// );
 
 const allowedOrigins = [
   "http://localhost:5173",
@@ -45,7 +33,6 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    credentials: true,
     origin: (origin, callback) => {
       console.log(origin);
       if (allowedOrigins.includes(origin!) || !origin) {
@@ -54,19 +41,23 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
+    credentials: true,
+    allowedHeaders: ["Content-Type, Authorization, X-Requested-With"],
+    methods: ["GET, POST, PUT, DELETE, OPTIONS"],
+    optionsSuccessStatus: 204,
   })
 );
 
-app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Requested-With"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.sendStatus(204);
-});
+// app.options("*", (req, res) => {
+//   res.header("Access-Control-Allow-Origin", req.headers.origin);
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, X-Requested-With"
+//   );
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.sendStatus(204);
+// });
 
 app.use(cookieParser());
 app.use("/api/v1/", route);
